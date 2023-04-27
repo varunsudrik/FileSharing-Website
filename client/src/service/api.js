c7d8d62dc12975da763a30d0b433e19fa7113ca8
Varun Sudrik <varunsudrik99@gmail.com>
2023-04-27 11:40:08 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URI = 'http://localhost:8000';
+const API_URI = "https://filesharing-mern.onrender.com:8000";
 
 export const uploadFile = async (data) => {
-    try {
-        const response = await axios.post(`${API_URI}/upload`, data);
-        return response.data;
-    } catch (error) {
-        console.log('Error while calling the API ', error.message);
-    }
-}
+  try {
+    const response = await axios.post(`${API_URI}/upload`, data);
+    return response.data;
+  } catch (error) {
+    console.log("Error while calling the API ", error.message);
+  }
+};
